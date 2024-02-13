810497f7b5b28ebc1d3527b03b42eccc1aee33d0
Jungjinah <abo8637@naver.com>
2024-02-13 21:29:43 +0900

    todolist

---
+++ main.js
@@ -0,0 +1,35 @@
+//user가 값을 입력한다.
+//+버튼을 클릭하면 할 일이 추가된다.
+//delete 버튼을 누르면 할 일이 삭제된다.
+//check 버튼을 누르면 할 일이 밑줄이 그어진다.
+//진행 중 끝남 탭을 누르면, 언더바가 이동한다.
+//끝난탭은 끝난 아이템만, 진행중 탭은 진행중 아이템만
+//전체탭을 누르면 다시 전체아이템으로 돌아옴
+
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+    let taskContent = taskInput.value;
+    taskList.push(taskContent);
+
+    render();
+}
+
+function render() {
+    let resultHTML = '';
+    for (let i = 0; i < taskList.length; i++) {
+        resultHTML += `<div class="task">
+        <div>${taskList[i]}</div>
+        <div>
+            <button>check</button>
+            <button>Delete</button>
+        </div>
+    </div>`;
+    }
+
+    document.getElementById("task-board").innerHTML = resultHTML;
+}
